33131026266fbd48724fd528a0e7b551a5b59fd8
Shardha123 <101158924+Shardha123@users.noreply.github.com>
2022-08-19 17:30:12 +0530

    Updated GridviewEdit Button changes
    Webform1.aspx ,Webform1.aspx .cs,Webform1.aspx.designer.cs are related to Gridview Edit demo changes

---
+++ WebForm1.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="GridView_Update.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="6" OnRowCancelingEdit="GridView1_RowCancelingEdit"   
+  
+OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating">  
+            <Columns>  
+                <asp:TemplateField>  
+                    <ItemTemplate>  
+                        <asp:Button ID="btn_Edit" runat="server" Text="Edit" CommandName="Edit" />  
+                    </ItemTemplate>  
+                    <EditItemTemplate>  
+                        <asp:Button ID="btn_Update" runat="server" Text="Update" CommandName="Update"/>  
+                        <asp:Button ID="btn_Cancel" runat="server" Text="Cancel" CommandName="Cancel"/>  
+                    </EditItemTemplate>  
+                </asp:TemplateField>  
+                <asp:TemplateField HeaderText="ID">  
+                    <ItemTemplate>  
+                        <asp:Label ID="lbl_ID" runat="server" Text='<%#Eval("ID") %>'></asp:Label>  
+                    </ItemTemplate>  
+                </asp:TemplateField>  
+                <asp:TemplateField HeaderText="Name">  
+                    <ItemTemplate>  
+                        <asp:Label ID="lbl_Name" runat="server" Text='<%#Eval("Name") %>'></asp:Label>  
+                    </ItemTemplate>  
+                    <EditItemTemplate>  
+                        <asp:TextBox ID="txt_Name" runat="server" Text='<%#Eval("Name") %>'></asp:TextBox>  
+                    </EditItemTemplate>  
+                </asp:TemplateField>  
+                <asp:TemplateField HeaderText="City">  
+                    <ItemTemplate>  
+                        <asp:Label ID="lbl_City" runat="server" Text='<%#Eval("City") %>'></asp:Label>  
+                    </ItemTemplate>  
+                    <EditItemTemplate>  
+                        <asp:TextBox ID="txt_City" runat="server" Text='<%#Eval("City") %>'></asp:TextBox>  
+                    </EditItemTemplate>  
+                </asp:TemplateField>  
+            </Columns>  
+            <%--<HeaderStyle BackColor="#663300" ForeColor="#ffffff"/>--%>  
+            <%--<RowStyle BackColor="#e7ceb6"/>--%>  
+        </asp:GridView>  
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GridView_Update
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+        SqlConnection con;
+        SqlDataAdapter adapt;
+        DataTable dt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ShowData();
+            }
+
+        }
+        //ShowData method for Displaying Data in Gridview  
+        protected void ShowData()
+        {
+            dt = new DataTable();
+            con = new SqlConnection(cs);
+            con.Open();
+            adapt = new SqlDataAdapter("Select ID,Name,City from tbl_Employee", con);
+            adapt.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            con.Close();
+        }
+        protected void GridView1_RowEditing(object sender, System.Web.UI.WebControls.GridViewEditEventArgs e)
+        {
+            //NewEditIndex property used to determine the index of the row being edited.  
+            GridView1.EditIndex = e.NewEditIndex;
+            ShowData();
+        }
+        protected void GridView1_RowUpdating(object sender, System.Web.UI.WebControls.GridViewUpdateEventArgs e)
+        {
+            //Finding the controls from Gridview for the row which is going to update  
+            Label id = GridView1.Rows[e.RowIndex].FindControl("lbl_ID") as Label;
+            TextBox name = GridView1.Rows[e.RowIndex].FindControl("txt_Name") as TextBox;
+            TextBox city = GridView1.Rows[e.RowIndex].FindControl("txt_City") as TextBox;
+            con = new SqlConnection(cs);
+            con.Open();
+            //updating the record  
+            SqlCommand cmd = new SqlCommand("Update tbl_Employee set Name='" + name.Text + "',City='" + city.Text + "' where ID=" + Convert.ToInt32(id.Text), con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            //Setting the EditIndex property to -1 to cancel the Edit mode in Gridview  
+            GridView1.EditIndex = -1;
+            //Call ShowData method for displaying updated data  
+            ShowData();
+        }
+        protected void GridView1_RowCancelingEdit(object sender, System.Web.UI.WebControls.GridViewCancelEditEventArgs e)
+        {
+            //Setting the EditIndex property to -1 to cancel the Edit mode in Gridview  
+            GridView1.EditIndex = -1;
+            ShowData();
+        }
+    }
+}
